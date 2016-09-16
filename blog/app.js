e949bf39946051c9553f2b3586908c59bb95b1db
szy0syz <185505508@qq.com>
2016-09-16 12:24:33 +0800

    modify

--- blog/app.js
+++ blog/app.js
@@ -9,12 +9,16 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// listen port
+app.set('port',process.env.PORT || 3000);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
